0bcc6987da900765abc2a9bf358b61bbbd5046bf
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 20:59:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,4 +4,15 @@
 # or whitespace (' ').
 
 class EmailAddressParser
+  attr_accessor :email_addresses 
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse (email_addresses)
+    data = email_addresses.split(/[, ]/)
+    data.uniq
+  end
+  
 end
